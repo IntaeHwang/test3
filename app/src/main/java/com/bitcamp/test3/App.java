79dcc7c0a03771de3c5e49f66f3d97d1d4713d93
js988174 <76925402+js988174@users.noreply.github.com>
2021-05-11 12:02:06 +0900

    ss

--- app/src/main/java/com/bitcamp/test3/App.java
+++ app/src/main/java/com/bitcamp/test3/App.java
@@ -10,7 +10,7 @@ public class App {
 
   public static void main(String[] args) {
     System.out.println("이야ㅑ야야ㅑ야야ㅑㅑㅑ");
-
+    System.out.println("gkgk");
     System.out.println("gkgk");
     System.out.println("되는건가");
     System.out.println("동인");
