f9a15befd073a4a748228fda9be876e949c38a75
js988174 <76925402+js988174@users.noreply.github.com>
2021-05-11 10:42:40 +0900

    .

--- app/src/main/java/com/bitcamp/test3/App.java
+++ app/src/main/java/com/bitcamp/test3/App.java
@@ -10,7 +10,7 @@ public class App {
 
   public static void main(String[] args) {
     System.out.println(new App().getGreeting());
-
+    System.out.println("점심 버거킹");
     System.out.println("bye~ b,y,e~");
   }
 }
