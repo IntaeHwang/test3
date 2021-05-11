e9643f2b89afdebd044e1787a31b0f56515f39dc
daehooon <22tjssu@naver.com>
2021-05-11 11:16:17 +0900

    ㅁㄴㅇ

--- app/src/main/java/com/bitcamp/test3/App.java
+++ app/src/main/java/com/bitcamp/test3/App.java
@@ -10,10 +10,10 @@ public class App {
 
   public static void main(String[] args) {
     System.out.println(new App().getGreeting());
-    System.out.println("햄버거는 맥도날드 :)");
     System.out.println("하하하하하하하");
     System.out.println("하하하하");
     System.out.println("하하하하하하하하하");
     System.out.println("하하");
+    System.out.println("안되네");
   }
 }
